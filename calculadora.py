a43c5fc152d2aaa2d5332abf319487543ce115be
Dahiana <dahiana_1186@hotmail.com>
2020-09-20 15:44:46 -0500

    SE SIGUE AÑADIENDO LA ESTRUCTURA DE LA CALCULADORA Y SE AÑADE LA FUNCION A

--- calculadora.py
+++ calculadora.py
@@ -7,4 +7,24 @@ ventana.title("Calculadora")
 ventana.geometry("400x600")
 ventana.resizable(False, False)
 ventana.configure(background="turquoise4")
+#SE SIGUE AÑADIENDO LA ESTRUCTURA DE LA CALCULADORA Y SE AÑADE LA FUNCION A
+color_boton="light slate gray"
+ancho_boton=10
+alto_boton=3
+operador = ""
+def limpiar():
+    global operador
+    operador = ""
+    texto_pantalla.set("0")
+def pulsar(A):
+    global operador
+    operador += str(A)
+    texto_pantalla.set(operador)
+def resultado():
+    global operador
+    try:
+        r = str(eval(operador))
+    except:
+        r = "Error"
+    texto_pantalla.set(r)
 
