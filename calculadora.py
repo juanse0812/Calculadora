f8e48a8ae8d5aec7ef8237cd79690d127e3aac48
Juan <juanse7594@gmail.com>
2020-09-20 15:57:45 -0500

    Se inserta el tipo de texto para la pantalla y se incluyen los primeros botones para la calculadora

--- calculadora.py
+++ calculadora.py
@@ -28,3 +28,10 @@ def resultado():
         r = "Error"
     texto_pantalla.set(r)
 
+#Se inserta el tipo de texto para la pantalla y se incluyen los primeros botones para la calculadora
+texto_pantalla = StringVar()
+#botones de la primera fila
+Boton0 = Button(ventana, text="0", background=color_boton, width=ancho_boton, height=alto_boton, command=lambda:pulsar(0)).grid(row=1, column=0, pady=10)
+Boton1 = Button(ventana, text="1", background=color_boton, width=ancho_boton, height=alto_boton, command=lambda:pulsar(1)).grid(row=1, column=1, pady=10)
+Boton2 = Button(ventana, text="2", background=color_boton, width=ancho_boton, height=alto_boton, command=lambda:pulsar(2)).grid(row=1, column=2, pady=10)
+Boton3 = Button(ventana, text="3", background=color_boton, width=ancho_boton, height=alto_boton, command=lambda:pulsar(3)).grid(row=1, column=3, pady=10)
