1434d0a31f7147cdcb4a5253df6777a08b4261eb
Juan <juanse7594@gmail.com>
2020-09-20 15:10:35 -0500

    Se añade la libreria con la estructura de la calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,9 @@
 #calculadora
+#se añade las librerias y se agrega la estructura de la calculadora
+from tkinter import *
+from math import *
+ventana = Tk()
+ventana.title("Calculadora")
+ventana.geometry("400x600")
+ventana.resizable(False, False)
+ventana.configure(background="turquoise4")
